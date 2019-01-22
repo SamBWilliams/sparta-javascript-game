e9a84f1fc37327fbbab8a8b70b528250de41539f
Sam Bowden-Williams <SBowdenWilliams@spartaglobal.com>
2019-01-22 22:36:18 +0000

    added platform class

--- js/test3.js
+++ js/test3.js
@@ -23,7 +23,7 @@ var Player = new (function (){
 	this.X = 0;
     this.Y = 0;
     
-    this.actualFrame = 0;
+   // this.actualFrame = 0;
 	
 	this.isJumping = 0;
 	this.isFalling = 0;
@@ -43,7 +43,76 @@ var Player = new (function (){
 			this.isJumping = true;
 			this.jumpSpeed = 22;
 		}
+    }
+    
+    this.checkJump = function() {
+		if(that.Y > height * 0.25) {
+			this.setPosition(that.X, that.Y - this.jumpSpeed);
+		} else {
+			if(this.jumpSpeed > 10) points += 100;
+			
+			platforms.forEach(function(platform, ind) {
+				platform.Y += this.jumpSpeed;
+				
+				if(platform.Y > height) {
+					platforms[ind] = new Platform(Math.random() * (width - platformWidth), platform.Y - height);
+				}
+			});
+        }
+
+        this.jumpSpeed--;
+		if(this.jumpSpeed == 0) {
+			this.isFalling = true;
+		}
+    }
+
+    this.checkFall = function() {
+		if(this.Y < height - this.height) {
+			this.setPosition(this.X, this.Y + this.fallSpeed);
+			this.fallSpeed++;
+		}else{
+			if(points == 0) {
+				that.fallStop();
+				}else{ 
+					GameOver(); //reload function
+				}
+		}
+    }
+    
+    this.fallStop = function() {
+		this.isFalling = false;
+		this.fallSpeed = 0;
+		this.jump();
+    }
+    
+    // that.moveLeft = function(theX) {
+	// 	if((that.X > 0) && that.isMoving) {
+	// 		that.setPosition(theX - that.width/2, that.Y);
+	// 	}
+	// }
+	
+	// that.moveRight = function(theX) {
+	// 	if((that.X + that.width < width) && that.isMoving) {
+	// 		that.setPosition(theX - that.width/2, that.Y);
+	// 	}
+    // }
+    
+    this.update = function() {
+
+		if(that.isJumping){
+            that.checkJump();
+        }else if(that.isFalling){
+            this.checkFall();  
+        }
+		 
+		this.draw();
+    }
+    
+    that.draw = function(){
+
+		ctx.drawImage(that.image, 0, that.height, that.width, that.height,that.X, that.Y, that.width, that.height);
 	}
+
 
 
 
