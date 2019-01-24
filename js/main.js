6d4433f527b2f173e0bb31f4464b4b8fcec0ad33
Sam Bowden-Williams <SBowdenWilliams@spartaglobal.com>
2019-01-24 22:08:59 +0000

    removed test files, moved updated code to main.js

--- js/main.js
+++ js/main.js
@@ -1,370 +1,353 @@
-console.log('working')
+var width = 1000
+var	height = 600
+var	canvas = document.getElementById('canvas')
+var	context = document.getElementsByTagName('canvas')[0].getContext('2d')
 
-//===PLAN====
-//Start menu
-//load page
-//Have character jump automatically
-//Load platforms
-//Upon impact with the platforms, character will accend whilst world decends, score will go up
-//Generate new platforms
-//As score reaches certain point platforms become more scarce
-//If player misses platform, game over
-
-//===Player class===
-//declare image
-//jump function
-//fall function
-
-// class Player(){
-
-// }
-
-
-var canvas = document.querySelector('canvas')
-
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-
-var context = canvas.getContext('2d')
-
-//keyboard input
-
-
-
-Player = function(x, y, gravity, w, h, counter, score){
+var	points = 0
+var gameState = "mainMenu"
     
-    //constructor
-    this.x = x;
-    this.y = y;
-    this.gravity = gravity;
-    this.w = w;
-    this.h = h;
-    this.jumpCounter = 15;
-    this.counter = counter;
-    this.gravity += 1;
-    this.score = score;
+var	background = 'lightblue' // Edit 
+var gameRunning;
+var gameLost = false;
+
+var nrOfPlatforms = 4;
+var	platforms = [];
+var	platformWidth = 80;
+var platformHeight = 5;
+    
+    
+var deatSnd = new Audio("sounds/0477.mp3")
+var jumpSnd = new Audio("sounds/bounce.mp3")
+
+title = new Image();
+title.src = "../img/jump.png";
+
+playerImg = new Image();
+playerImg.src = "../img/pogo.png"
+
+	
+canvas.width = width;
+canvas.height = height;
 
 
-    // this.jump = function(){
+
+var clear = function(){
+	context.fillStyle = background; // Edit
+	context.beginPath();
+	context.rect(0,0, width, height);
+	context.closePath();
+	context.fill();
+};
+
+//Change to class poss
+var player = new (function(){
+	
+	
+	this.width = 70;
+    this.height = 80;
+    
+    this.imgHeight = 126;
+	
+	this.X = 0;
+	this.Y = 0;
+	
+	this.jumping = 0;
+	this.falling = 0;
+	
+	this.jumpSpeed = 0;
+	this.fallSpeed = 0;
+
+    this.isMoving = true;
+    
+    var platformCaller = this;
+	
+	this.setPosition = function(x, y){
+		this.X = x;
+		this.Y = y;
+	}
+	
+	//Constant jump
+	this.jump = function() {
+		if(!this.falling) {
+			this.jumping = true;
+            this.jumpSpeed = 22;
+            jumpSnd.play();
+		}
+	}
+    
+    
+	this.checkJump = function() {
+		if(this.Y > height * 0.30) {
+			this.setPosition(this.X, this.Y - this.jumpSpeed);
+		} else {
+			if(this.jumpSpeed > 10) //revisit
+			
+			platforms.forEach(function(platform, ind) {
+				platform.Y += platformCaller.jumpSpeed;
+				
+				if(platform.Y > height) {
+                    platforms[ind] = new Platform(Math.random() * (width - platformWidth), platform.Y - height);
+                    points ++;
+				}
+			});
+		}
+		
+		this.jumpSpeed--;
+		if(this.jumpSpeed == 0) {
+			this.falling = true;
+		}
+	}
+	
+	this.checkFall = function() {
+		if(this.Y < height - this.height) {
+			this.setPosition(this.X, this.Y + this.fallSpeed);
+			this.fallSpeed++;
+		} else{
+                deatSnd.play(); 
+                gameSt("gameover")
+		    }
+	}
+	
+	this.stopFall = function() {
+		this.falling = false;
+		this.fallSpeed = 10;
+		this.jump();
+	}
+		
+	
+	this.moveLeft = function(mouseX) {
+		if((this.X > 0) && this.isMoving) {
+			this.setPosition(mouseX - this.width/2, this.Y);
+		}
+	}
+	
+	this.moveRight = function(mouseX) {
+		if((this.X + this.width < width) && this.isMoving) {
+            this.setPosition(mouseX - this.width/2, this.Y);
+            //playerImg.src = "../img/pogoR.png"
+		}
+	}
+	
+	this.update = function() {
+		if(this.jumping) this.checkJump();
+		if(this.falling) this.checkFall();
+		this.draw();
+	}
+		
+	
+	this.draw = function(){
+
+        // context.fillStyle = "red";
+        // context.fillRect(this.X, this.Y, this.width, this.height);
+        context.drawImage(playerImg, this.X, this.Y, this.width, this.height)
+
+	}
+})();
+
+var Platform = function(x, y) {
+
+	
+	this.onCollide = function() {
+        player.stopFall();
         
-    //     if (this.y + this.h > basePlatform.y){ //change canvas to platform
-    //         this.gravity = -this.gravity;
-    //     }
-    //     //Allows player to jump on platform
-        
-
-    //     //FOR EACH JUMP ADD TO PLATFORM ARRAY TO GENERATE PLATFORM
-        
-
-    //     platformArray.forEach(randomPlatform => {
-            
-    //         if (this.y + this.h > randomPlatform.y && this.x >= randomPlatform.x && this.x <= randomPlatform.x + randomPlatform.w && this.gravity >0.8){ 
-            
-    //             this.gravity = -this.gravity;
-
-    //             platformArray.forEach(randomPlatform =>{
-    //                 randomPlatform.y +=20;
-
-    //             })
-    //             //this.jumpCounter++;
-                
-                
-    //         } else{
-    //             this.gravity += 0.8;
-    //             this.gravity -= 0.7;
-    //             // if(this.y + this.h < randomPlatform.y){}
-    //             //     this.gravity -= 0.8;
-    //             // }
-    //         }
-            
-    //     });
-
-    //     //add jump counter
-    // }
+	}
+	
+	
+	this.draw = function() {
+		context.fillStyle = 'darkgreen';
+		context.fillRect(this.X, this.Y, platformWidth, platformHeight);
+	}
+	
+	
+	this.X = x;
+	this.Y = y + 50;
+	
+	return this;
+};
 
 
-    this.platformSpawner = function(){
-        
-        platformArray.forEach(randomPlatform => {
-            
-            if (this.y + this.h > randomPlatform.y && this.x >= randomPlatform.x && this.x <= randomPlatform.x + randomPlatform.w && this.gravity >0.8){ 
-                // && this.gravity >0.8
-            
-                this.gravity = -this.gravity;
-                this.counter ++;
-                //this.jump();
+var generatePlatforms = function() {
 
-                platformArray.forEach(randomPlatform =>{
-                    randomPlatform.y +=5;
+    //Sets Y position to top of canvas
+	var position = 0;
+	
+	for(var i = 0; i < nrOfPlatforms; i++) {
+		
+		platforms[i] = new Platform(Math.random()*(width-platformWidth), position);
+		
+		if(position < height - platformHeight) position += (height / nrOfPlatforms);
+	}
+}();
 
-                })
-                //this.jumpCounter++;
-                
-                
-            } else{
-                this.gravity += 0.8;
-                this.gravity -= 0.7;
-                this.score ++;
-                // if(this.y + this.h < randomPlatform.y){}
-                //     this.gravity -= 0.8;
-                // }
-            }
-            
-        });
 
-        //add jump counter
+//Collision detection
+var checkCollision = function() {
+	platforms.forEach(function(e) {
+        if((player.falling)&&(player.X + player.width > e.X)&&
+        (player.Y + player.height > e.Y)&&(player.X < e.X + platformWidth)
+		  ){
+			e.onCollide();
+		}
+	})
+}
+
+
+document.onmousemove = function(e) {
+	if(gameState == "game") {
+		if(player.X - 10) {
+			player.moveLeft(e.pageX);
+		} else if(player.X - 10) {
+			player.moveRight(e.pageX);
+		}
+	}
+};
+
+
+document.onmousedown = function() {
+	if(gameState == "mainMenu") {
+        gameState = "game";
+       
+    }else if(gameState == false){
+        gameState = "mainMenu";
     }
+   
+}
 
-    this.gameOver = function(){
-        if(this.y > 800){
-            alert("Game over")
+// player.setPosition(((width - player.width) / 2), ((height - player.height) / 2));
+player.setPosition(((width - player.width) / 2), ((height - player.height) / 2));
+player.jump();
+
+var gameLoop = function() {
+	clear();
+	
+	platforms.forEach(function(platform){
+		platform.draw();
+	});
+	
+	checkCollision();
+	
+	player.update();
+	if(gameState){
+		gameRunning = setTimeout(gameLoop, 1000 / 50);
+    }
+    
+    context.font = "20pt Arial";
+	context.fillStyle = "Black";
+	context.fillText("Points: " + points, width / 2 - 500, height / 2 - 250);
+}
+
+var StartMenu = function() {
+    clear();
+
+    
+
+    context.drawImage(title, width/ 2 - 240, (height/ 2) - 250)
+	
+	context.font = "30pt Arial";
+	context.fillStyle = "green";
+    context.fillText("Click anywhere to play", width / 2 - 210, (height / 2) - -60);
+    
+    context.font = "20pt Arial";
+	context.fillStyle = "black";
+    context.fillText("Press 'I' for instructions", width / 2 - 150, (height / 2) - -250);
+	
+	if(gameState == "mainMenu"){
+        gameRunning = setTimeout(StartMenu, 1000 / 50); 
+    }else {
+		clearTimeout();
+		gameLoop();
+    }
+    
+    document.addEventListener('keydown', function(){
+        var key_press = String.fromCharCode(event.keyCode);
+        if(key_press == "I"){
+            //  context.fillText("Instructions", width / 2 - 150, (height / 2) - -250)
+            instMenu()
         }
-    }
-
-    // this.jump = function(){
-    //     this.y += 5;
-    // }
-    
-
-
-
-
-    this.update = function(){
-        //this.gameOver();
-        this.platformSpawner()
-        //this.jump();
-        this.y += this.gravity
-        
-        this.draw();
-    }
-
-
-    this.draw = function(){
-        context.fillRect(this.x, this.y, this.w, this.h)
-
-    }
+    })
 }
 
-class Game{
-    constructor(){
+var gameOver = function() {
+	
+     gameState = false;
+     clearTimeout(gameLoop);
+     gameLost = true;
 
-    }
+    setTimeout(function() {
+        	clear();
+        	context.fillStyle = "Red";
+        	context.font = "30pt Arial";
+            context.fillText("GAME OVER!", width / 2 - 150, height / 2 - 80);
 
-    // platFormGenerator(){
+            context.font = "20pt Arial"
+            context.fillStyle = "Green"
+            context.fillText("YOU SCORED: " + points + " POINTS", width / 2 - 185, height / 2 - 35);
 
-    // }
+            context.font = "15pt Arial"
+            context.fillStyle = "Black"
+            context.fillText("Press spacebar to restart",width / 2 - 140, height / 2 )
+
+            document.addEventListener('keydown', function(e){
+                var key_press = String.fromCharCode(event.keyCode);
+                if(key_press == " "){
+                    clear()
+                    location.reload(); 
+                }
+            })
+    
+        }, 100);
+    
+    
 }
 
-
-Platform = function(x,y,w,h){
-    
-    //constructor
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-
-    this.update = function(){
-
-        this.y +=2;
-
-        // setInterval(function(){
-        //     this.draw(); 
-        // },3000)
-        this.draw();
-        
-        // if (this.y > 800){
-        //     initPlatforms();
-        // }
-
-        
+var gameSt = function(state){
+    if(state == "mainMenu"){
+        clear();
+        StartMenu();
+    }else if(state == "gameover"){
+        clear();
+        gameOver();
     }
 
-    this.draw = function(){
-
-    //     setInterval(function(){
-    //     for (let i = 0; i < platformArray.length; i++) {
-    //         context.fillRect(this.x, this.y, this.w, this.h)
-            
-    //     }  
-    // }, 3000)
-
-    // let x = Math.random() * canvas.height;
-    // let y = Math.random() * canvas.width;
-    // test = new Platform(this.x = x, this.y = y, this.w = 200, this.h = 20) //Constant platform spawn
-
-    context.fillRect(this.x, this.y, this.w, this.h)
-
-    }
 }
 
+var instMenu = function(){
+    clear()
+    gameState = false;
+     //clearTimeout(StartMenu);
+    // gameLost = true;
 
-var player;
-var platform;
-var platform2;
-var basePlatform;
-var randomPlatform;
-let platformArray = []
-let yArray = [];
+    context.font = "30pt Arial"
+    context.fillStyle = "Black"
+    context.fillText("Welcome to Jump!",width / 2 - 200, (height / 2) - 200)
 
-function init(){
-    player = new Player(400, 200, 10, 30, 60, 0, 0)
-    platform = new Platform(500,500,200,20)
-    platform2 = new Platform(800, 300, 200, 20)
-    basePlatform = new Platform(0, 650, 1000, 20)
+    context.font = "20pt Arial"
+    context.fillStyle = "Black"
+    context.fillText("1. Use the mouse to move left and right",width / 2 - 270, (height / 2) - 100)
 
-    
-     for (let i = 0; i < 15; i++) {
-            let x = Math.random() * canvas.height;
-            let y = Math.random() * canvas.width;
-           // let y = 
-            randomPlatform = new Platform(x, y, 200, 20)
-            platformArray.push(randomPlatform)
-            yArray.push(randomPlatform.w)
-     }
+    context.font = "20pt Arial"
+    context.fillStyle = "Black"
+    context.fillText("2. Jump from platform to platform to gain points",width / 2 - 270, (height / 2) - 50)
 
-    console.log(platformArray)
-}
-
-// function initPlatforms(){
-//     for (let i = 0; i < 1; i++) {
-//         let x = Math.random() * canvas.height;
-//         let y = Math.random() * canvas.width;
-//        // let y = 
-//         randomPlatform = new Platform(x, y, 200, 20)
-//         platformArray.push(randomPlatform)
-//  }
-// }
+    context.font = "20pt Arial"
+    context.fillStyle = "Black"
+    context.fillText("3. Don't look down!",width / 2 - 270, (height / 2) - 0)
 
 
+    context.font = "15pt Arial"
+    context.fillStyle = "Black"
+    context.fillText("Press spacebar to go back",width / 2 - 140, (height / 2) - -200)
 
-function animate(){
-    requestAnimationFrame(animate);
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    player.update()
-    //platform.update();
-    //platform2.update();
-    basePlatform.update();
-    //randomPlatform.update(); - faster moving platform
-    
-    // if(player.counter =  3){
-    //     for (let i = 0; i < platformArray.length; i++) {
-    //         platformArray[i].update();
-            
-    //     }
-    // }
-
-    // setInterval(function (){
-    //     test.update();
-    // }, 10000)
-    //test.update();
-
-    // randomPlatform.update()
-    // setInterval(function(){
-    //     for (let i = 0; i < platformArray.length; i++) {
-    //         platformArray[i];
-
-    //         //console.log(platformArray[i])
-            
-            
-    //     }  
-    // }, 3000)
-
-   // console.log(platformArray)
-    //    let num = 0;
-    //    let num2 = 0;
-
-    //    while (num != 2){
-    //        if (player.y =200){
-    //         for (let i = 0; i < platformArray.length; i++) {
-    //             platformArray[i].update();
-    //         }
-    //        }
-            
-    //     }
-
-   //}
-
-//    if (platformArray[0].y > 500){
-    for (let i = 0; i < platformArray.length; i++) {
-        if (player.y === i*600){
-            platformArray.update();
-           }
-
-   }
-
-   
-
-   
-
-//    if (player.y === i*600){
-//     platformArray[i].update();
-//    }
+    document.addEventListener('keydown', function(e){
+        var key_press = String.fromCharCode(event.keyCode);
+        if(key_press == " "){
+            clear()
+            location.reload(); 
+        }
+    })
 
 
-
-        // for (let i = 0; i < platformArray.length; i++) {
-        //         platformArray[i].update();
-        // }
-        
-       // platformArray[i].update();
-
-        //requestAnimationFrame(platformSpawner2)
-        
 }
 
 
-    
-    
-
-// var platformSpawner2 = function(){
-//     setInterval(function(){
-//         for (let i = 0; i < platformArray.length; i++) {
-//             platformArray[i].update();
-            
-//         }  
-//     }, 3000)
-// }
-
-
-
-//=======MOVEMENT=======
-document.addEventListener('keydown', function(e){
-    var key_press = String.fromCharCode(event.keyCode);
-
-    if(key_press == "A"){
-        player.x -= 50;
-       // console.log(platformArray[0])
-    } else if(key_press == "D"){
-        player.x += 50;
-    }
-})
-
-// function maxLength(array){
-//     let x = array[0];
-//     for (let i = 1; i < array.length; i++){
-//         if (array[i] > x){
-//           x = array[i];
-//         }
-//     }
-//     return x
-//   }
-  
-//   maxLength(yArray)
-//   console.log(maxLength(yArray))
-
-
-init();
-//initPlatforms();
-animate();
-// platformSpawner2();
-// maxLength(yArray)
-//   console.log(maxLength(yArray))
-//   console.log(yArray)
-
-
-
-
-
-
+gameSt(gameState);
